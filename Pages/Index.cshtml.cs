0284b3d951a7439e5d9d54732fc6584ea37bd526
lancelim8122 <lancelim8122@gmail.com>
2024-03-26 22:18:51 +0800

    Updated greeting

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace az_webapp_lance.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
         public void OnGet()
         {
-
+            ViewData["Greeting"] = configuration["Greeting"];
         }
     }
 }
