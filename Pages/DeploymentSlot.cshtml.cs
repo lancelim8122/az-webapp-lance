e22ff5db40f452b9583e51641e11299888eb4e35
lancelim8122 <lancelim8122@gmail.com>
2024-03-26 22:50:04 +0800

    DeploymentSlot changes

---
+++ Pages/DeploymentSlot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace az_webapp_lance.Pages
+{
+    public class DeploymentSlotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
